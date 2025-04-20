c482b9f5ae720144b5d1cca63b80d190d86892cc
sdhajwane <102373590+sdhajwane@users.noreply.github.com>
2025-04-20 11:53:52 +0530

    Delete src/Components directory

--- src/Components/LoadingScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function LoadingScreen() {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-        <img src="/logo.svg" alt="Logo" className=" animate-pulse" />
-      </div>
-    );
-  }
-  
